cdc3d733f395d06a0f78f4efea088d8817951d27
chandanapractice <124656681+chandanapractice@users.noreply.github.com>
2023-02-07 12:54:41 +0530

    Update Test1.java

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -3,7 +3,7 @@ package tests;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("This is my first git program");
+		System.out.println("This is our first git program");
 
 	}
 
